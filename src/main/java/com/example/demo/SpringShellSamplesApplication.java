05fd531a19306f361dd733469311697ef6ee20c9
chenzhenyang <czy_1@qq.com>
2019-05-04 22:29:51 +0800

    init

---
+++ src/main/java/com/example/demo/SpringShellSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellSamplesApplication.class, args);
+	}
+
+}
